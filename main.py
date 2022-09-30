44fe67e780dfe31170722c04f05d741da96c11eb
Oranx <xiangxiaoc@outlook.com>
2022-09-30 10:35:37 +0800

    update

--- main.py
+++ main.py
@@ -2,11 +2,18 @@ import typer
 
 import util.log
 
+# 在任意目录执行脚本时，python 进程都会临时切换到脚本所在目录再执行
+dir_path, script_name = os.path.split(__file__)
+if dir_path != '':
+    os.chdir(dir_path)
+
 app = typer.Typer()
+
 
 @app.command()
 def version():
     print('version')
+
 
 @app.command()
 def hello(name: str):
@@ -14,10 +21,7 @@ def hello(name: str):
 
 
 if __name__ == '__main__':
-
     logger = util.log.get_logger()
     logger.info('123')
-
-
 
     app()
